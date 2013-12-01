0353e283b52697e6b9de4f93e2b7225e2bdc9705
DanieleGravina <daniele.gravina@mail.polimi.it>
2013-12-01 19:28:06 +0100

    Initial draft of textual game

---
+++ theTutorial/Assets/MenuGame.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum MenuState{
+	AUDIO,
+	CONTROLS, 
+	ACHIEVEMENTS,
+	EXIT
+}
+
+public class MenuGame : MonoBehaviour {
+	
+	public GameObject InitialMenu;
+	public GameObject ExitMenu;
+	
+	Vector3 positionMenu = new Vector3(0.6936188f+165f, -74.67452f, 24.47535f);
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	// Change the state of menu text, called(buttons) by children on trigger events
+	public void ChangeState(MenuState actualState){
+		
+		switch(actualState){
+			
+		case MenuState.EXIT: 
+			Destroy(InitialMenu);
+			Instantiate(ExitMenu, positionMenu, Quaternion.identity);
+			break;
+		}
+	}
+}
---
+++ theTutorial/Assets/Script/Button.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Button : MonoBehaviour {
+	
+	public MenuState myState;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnTriggerEnter(Collider other){
+		
+		//problem if parent destroy this object during the call ??
+		if(other.tag == "Player"){
+			transform.parent.transform.parent.GetComponent<MenuGame>().ChangeState(myState);
+		}
+	}
+}
