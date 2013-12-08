83c3cea2b5df502fd29b5aebc389bbe1b2ec4778
Alessandro <sgabry90@gmail.com>
2013-12-08 18:08:31 +0100

    add timer cube and the script to make the cube clickable

---
+++ theTutorial/Assets/Script/TimerReset.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimerReset : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void OnMouseOver () {
+		if (Input.GetMouseButtonUp(0)){
+			Debug.Log("click done");
+		}
+	}
+}
