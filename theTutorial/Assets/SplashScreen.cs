ac0a3079d7cce0ea899f9d006466de8b44dad4a2
DanieleGravina <daniele.gravina@mail.polimi.it>
2014-01-23 11:39:26 +0100

    Added SplashScreen

---
+++ theTutorial/Assets/SplashScreen.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SplashScreen : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		Invoke ("LoadMenu", 1f);
+	
+	}
+	
+	void LoadMenu(){
+		Application.LoadLevel("InitialMenu");
+	}
+}
